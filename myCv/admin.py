0c654eb006660b8dbf67091bd61d29719f63e33e
Melek TEKE <melek.teke@stu.pirireis.edu.tr>
2024-05-13 02:39:51 +0300

    feat : add general setting model to admin

--- myCv/admin.py
+++ myCv/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from myCv.models import *
 
+
+@admin.register(GeneralSetting)
 # Register your models here.
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'parameters', 'updated_date', 'created_date']
+    search_fields = ['name','description','parameters']
+    list_editable = ['description','parameters']
+
+    class Meta:
+        model = GeneralSetting
